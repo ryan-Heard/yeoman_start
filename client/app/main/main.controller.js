31585c64c157dba49c49e06233a382f55428468f
Ryan Heard <brotherakuma@gmail.com>
2016-01-05 23:06:51 -0600

    Updating serveral parts

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,11 +7,37 @@ class MainController {
   constructor($http) {
     this.$http = $http;
     this.awesomeThings = [];
+    this.myInterval = 5000;
+    this.noWrapSlides = false;
+    var slides = this.slides = [];
+
+    for (var i=0; i<4; i++) {
+      addSlide();
+    }
+};
+
+  addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
 
     $http.get('/api/things').then(response => {
       this.awesomeThings = response.data;
     });
   }
+
+  .addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
 
   addThing() {
     if (this.newThing) {
